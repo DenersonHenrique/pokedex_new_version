563b17326f73c2febc4ff10041f2559aea478b50
DenersonHenrique <denersonh@gmail.com>
2024-02-29 18:41:36 -0300

    Update test.

--- test/modules/home/domain/usecases/get_pokemon_list_usecase_test.dart
+++ test/modules/home/domain/usecases/get_pokemon_list_usecase_test.dart
@@ -11,28 +11,6 @@ class PokedexHomeRepositoryMock extends Mock
 void main() {
   late PokedexHomeRepositoryMock repository;
   late GetPokemonList usecase;
-  final List<PokemonEntity> pokemonList = <PokemonEntity>[
-    PokemonEntity(
-      id: faker.hashCode,
-      num: faker.randomGenerator.integer(150).toString(),
-      name: faker.guid.guid(),
-      img: faker.guid.guid(),
-      type: faker.lorem.words(4),
-      height: faker.guid.guid(),
-      weight: faker.guid.guid(),
-      candy: faker.guid.guid(),
-      candyCount: faker.randomGenerator.integer(25),
-      egg: faker.guid.guid(),
-      spawnChance: faker.randomGenerator.decimal(),
-      avgSpawns: faker.randomGenerator.decimal(),
-      spawnTime: faker.guid.guid(),
-      multipliers: faker.randomGenerator.amount(
-        (_) => faker.randomGenerator.decimal(),
-        10,
-      ),
-      weaknesses: faker.lorem.words(4),
-    )
-  ];
 
   setUp(() {
     repository = PokedexHomeRepositoryMock();
@@ -42,13 +20,36 @@ void main() {
   group('Find Pokemon list.', () {
     test('Should be return a Pokemon List.', () async {
       when(repository.getPokemonList()).thenAnswer(
-        (_) => Future.value(pokemonList),
+        (_) async => Future.value(pokemonList),
       );
 
       final result = await usecase.call();
 
-      expect(result, pokemonList);
+      expect(result, equals(pokemonList));
       verify(() => repository.getPokemonList()).called(1);
     });
   });
 }
+
+final List<PokemonEntity> pokemonList = <PokemonEntity>[
+  PokemonEntity(
+    id: 1,
+    num: faker.randomGenerator.integer(150).toString(),
+    name: faker.guid.guid(),
+    img: faker.guid.guid(),
+    type: faker.lorem.words(4),
+    height: faker.guid.guid(),
+    weight: faker.guid.guid(),
+    candy: faker.guid.guid(),
+    candyCount: faker.randomGenerator.integer(25),
+    egg: faker.guid.guid(),
+    spawnChance: faker.randomGenerator.decimal(),
+    avgSpawns: faker.randomGenerator.decimal(),
+    spawnTime: faker.guid.guid(),
+    multipliers: faker.randomGenerator.amount(
+      (_) => faker.randomGenerator.decimal(),
+      10,
+    ),
+    weaknesses: faker.lorem.words(4),
+  )
+];
