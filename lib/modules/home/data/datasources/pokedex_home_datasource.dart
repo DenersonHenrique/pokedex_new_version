2d2f824080a3ca954c5650c3f64e2b7d498e5098
DenersonHenrique <denersonh@gmail.com>
2024-03-01 15:41:44 -0300

    [development] Create DataSource implementation. Http adapters.

--- lib/modules/home/data/datasources/pokedex_home_datasource.dart
+++ lib/modules/home/data/datasources/pokedex_home_datasource.dart
@@ -1,5 +1,27 @@
-import 'package:pokedex_new_version/modules/home/data/models/pokemon_model.dart';
+import 'package:pokedex_new_version/commons/helpers/erros.dart';
+
+import '../../../../commons/adapters/http_client.dart';
+import '../models/pokemon_model.dart';
 
 abstract class IPokedexHomeDatasource {
   Future<List<PokemonModel>> get();
+}
+
+class PokedexHomeDataSource extends IPokedexHomeDatasource {
+  final IHttpClient _client;
+
+  PokedexHomeDataSource(this._client);
+
+  @override
+  Future<List<PokemonModel>> get() async {
+    try {
+      final response = await _client.get('path');
+
+      return ((response.data ?? []) as List)
+          .map((element) => PokemonModel.fromMap(element))
+          .toList();
+    } on Exception catch (error) {
+      throw DatasourceException(exception: error);
+    }
+  }
 }
